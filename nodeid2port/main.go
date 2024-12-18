33746442fa04da4eb0da24d862a2695440c1d2ad
bianyuanop <chen.me.nan@gmail.com>
2024-12-18 10:04:03 -0500

    go.work update

--- nodeid2port/main.go
+++ nodeid2port/main.go
@@ -15,8 +15,9 @@ func main() {
 	// starting val server for node(NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg) at port: 33549
 	id, err := ids.NodeIDFromString(nodeIDString)
 	if err != nil {
+		fmt.Printf("unable to parse nodeID: %s\n", err.Error())
 		os.Exit(1)
 	}
 	port := utils.GetPortFromNodeID(id)
-	fmt.Println(port)
+	fmt.Printf("%d", port)
 }
